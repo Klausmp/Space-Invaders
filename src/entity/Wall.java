b0fa997bc3f1ce69fba1860ac3e41e77160ea8a1
Klausmp <cartvdennis@gmail.com>
2018-07-01 16:50:47 +0200

    Add Wall Class

---
+++ src/entity/Wall.java
@@ -0,0 +1,23 @@
+package entity;
+
+public class Wall extends Entity {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void loadAndSetTextures() {
+
+    }
+
+    @Override
+    public void movement() {
+
+    }
+
+    @Override
+    public void animation() {
+
+    }
+}
