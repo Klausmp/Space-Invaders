35a0781f217a6ca58e41f26832627f907dbbabc2
Klausmp <cartvdennis@gmail.com>
2018-10-19 12:29:16 +0200

    Dokumentation

--- src/entity/AlienBullet.java
+++ src/entity/AlienBullet.java
@@ -9,10 +9,13 @@ import world.World;
  */
 
 public class AlienBullet extends Bullet {
+    //Legt fest wie lange die Todesanimation nach einem Treffer angezeigt wird
     public int deadAnimationTimer = 0;
     public final int DEADANIMATIONTIMER = 7;
+    //Leget fest in welchem abstand die Texturen für die Animation geändert wird
     public int animationTimer = 0;
     public final int ANIMATIONTIMER = 6;
+    //Speichert den Momentanen Status der Animation (welche der beiden Texturen gerade angezeigt wird)
     public boolean animationStatus = true;
 
     public AlienBullet(int posX, int posY) {
@@ -23,15 +26,17 @@ public class AlienBullet extends Bullet {
     public void hit() {
         for (World world: Util.getWorldList()) {
             for (Player player: world.getPlayerList()) {
+                //Prüfung ob die Hitbox des Bullet sich mit der des Spielers Überschneidet
                 if (getBounding().intersects(player.getBounding())){
+                    //Gibt das Bullet zur entfernung frei
                     this.setCanBeRemoved(true);
+                    //Tötet den Spieler
                     player.setAlive(false);
-
                 }
             }
         }
-
-        hitWall();
+        //Prüft ob ein Shield getroffen wird
+        hitShield();
     }
 
     @Override
@@ -41,36 +46,45 @@ public class AlienBullet extends Bullet {
 
     @Override
     public void movement() {
+        //Testet ob das Bullet am leben ist
         if (isAlive()) {
+            //Lässt den Schuss mit dem Speed nach unten fallen (plus auf der Y-Achse)
             setPosY(getPosY() + speed);
         }
-
+        //Testet ob der Schuss das Fenster am unteren ende (Positive Y Richtung)
         if ((int) getPosY() >= Renderer.getWindowSizeY()) {
+            //Tötet den Schuss
             this.setAlive(false);
         }
     }
 
     @Override
     public void animation() {
+        //Erhöht den wert des AnimationTImers um 1
         setAnimationTimer(getAnimationTimer() + 1);
+        //Früft ob der Delay zwischen dem Wechsel der Texturen zu ende ist
         if (getANIMATIONTIMER() == getAnimationTimer()) {
+            //Negiert die Texturen der Animation
             if (isAnimationStatus()) {
                 look = Renderer.getAlienBullet0();
                 setAnimationStatus(false);
-                setAnimationTimer(0);
             } else {
                 look = Renderer.getAlienBullet1();
                 setAnimationStatus(true);
-                setAnimationTimer(0);
             }
+            //Startet den Delay zwischen den Wechseln der Textur von neuem
+            setAnimationTimer(0);
         }
-
+        //Prüfung ob der Schuss nicht am leben ist
         if (!isAlive()) {
+            //Setzt die Textur auf die Todestextur
             look = Renderer.getAlienBulledDead();
+            //Erhöt den TodesTimer um 1
             setDeadAnimationTimer(getDeadAnimationTimer() + 1);
         }
-
+        //Prüft ob die Todesanimation abgeschlossen ist
         if (getDEADANIMATIONTIMER() <= getDeadAnimationTimer()) {
+            //Gibt den Schuss zum Löschen frei
             setCanBeRemoved(true);
         }
     }
